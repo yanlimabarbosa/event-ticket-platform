b2b4833c1a149a1d74df9a3c7407bd38866f95e9
Yan Lima Barbosa <yanbr763@gmail.com>
2025-08-17 12:56:18 -0300

    feat: add User entity with fields for id, name, email, and timestamps

---
+++ src/main/java/com/yan/tickets/domain/User.java
@@ -0,0 +1,45 @@
+package com.yan.tickets.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    // TODO: Organized events
+
+    // TODO: Attending events
+
+    // TODO: Staffing events
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
