d21fc639702c346ce09985a125547573a9dba1c3
Yan Lima Barbosa <yanbr763@gmail.com>
2025-09-15 20:48:33 -0300

    feat: implement UserProvisioningFilter to automatically provision users from JWT authentication

---
+++ src/main/java/com/yan/tickets/filters/UserProvisioningFilter.java
@@ -0,0 +1,48 @@
+package com.yan.tickets.filters;
+
+import com.yan.tickets.domain.User;
+import com.yan.tickets.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UserProvisioningFilter extends OncePerRequestFilter {
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && authentication.getPrincipal() instanceof Jwt jwt) {
+            UUID keycloakId = UUID.fromString(jwt.getSubject());
+
+            if(!userRepository.existsById(keycloakId)){
+                User user = new User();
+                user.setId(keycloakId);
+                user.setName(jwt.getClaimAsString("preferred_username"));
+                user.setEmail(jwt.getClaimAsString("email"));
+
+                userRepository.save(user);
+            }
+
+            filterChain.doFilter(request, response);
+        }
+    }
+}
