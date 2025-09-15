2aba9d9e9e6484da7b5862ac51a1b9b8980ee8c1
Yan Lima Barbosa <yanbr763@gmail.com>
2025-09-15 20:04:44 -0300

    feat: introduce QrCode entity with fields for id, status, value, and relationships to Ticket

---
+++ src/main/java/com/yan/tickets/domain/QrCode.java
@@ -0,0 +1,41 @@
+package com.yan.tickets.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "qr_codes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class QrCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private QrCodeStatusEnum statusEnum;
+
+    @Column(name = "value", nullable = false)
+    private String value;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
