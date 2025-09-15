a5e0e2935040064f64d159722697908dbe8bce6b
Yan Lima Barbosa <yanbr763@gmail.com>
2025-09-15 20:48:24 -0300

    feat: create UserRepository interface for User entity management using JpaRepository

---
+++ src/main/java/com/yan/tickets/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yan.tickets.repositories;
+
+import com.yan.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
